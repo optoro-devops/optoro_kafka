4ad6f572c6831d65fefe43d0c7947a516806daab
Zach Dunn <zdunn@optoro.com>
2015-02-12 15:35:38 -0500

    Initial commit

--- recipes/test.rb
+++ recipes/test.rb
@@ -1,1 +1,7 @@
-node.set['fqdn'] = 'test-kafka-001.optoro.com'
+# node.set['fqdn'] = 'test-kafka-001.optoro.com'
+bash 'set hostname' do
+  user 'root'
+  code <<-EOH
+  echo 127.0.0.1 `hostname` >> /etc/hosts
+  EOH
+end
--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -6,27 +6,31 @@ describe 'optoro_kafka::default' do
 
         let(:chef_run) do
           ChefSpec::SoloRunner.new(platform: platform, version: version, log_level: :error) do |node|
+            env = Chef::Environment.new
+            env.name 'test-kitchen'
+            allow(node).to receive(:chef_environment).and_return(env.name)
+            allow(Chef::Environment).to receive(:load).and_return(env)
             node.set['lsb']['codename'] = value['codename']
           end.converge(described_recipe)
-
-          it 'includes apt' do
-            expect(chef_run).to include_recipe('apt')
-          end
-          it 'includes exhibitor' do
-            expect(chef_run).to include_recipe('exhibitor')
-          end
-          it 'includes cerner_kafka' do
-            expect(chef_run).to include_recipe('cerner_kafka')
-          end
-          it 'includes cerner_kafka::offset_monitor' do
-            expect(chef_run).to include_recipe('cerner_kafka::offset_monitor')
-          end
-          it 'should delay in restarting kafka' do 
-            #resource = chef_run.execute('untar kafka binary')
-            expect(run_resource('untar kafka binary')).to notify('service[kafka]').to(:restart).delayed
-          end
-
         end
+
+        it 'includes apt' do
+          expect(chef_run).to include_recipe('apt')
+        end
+        it 'includes exhibitor' do
+          expect(chef_run).to include_recipe('exhibitor')
+        end
+        it 'includes cerner_kafka' do
+          expect(chef_run).to include_recipe('cerner_kafka')
+        end
+        it 'includes cerner_kafka::offset_monitor' do
+          expect(chef_run).to include_recipe('cerner_kafka::offset_monitor')
+        end
+        it 'should delay in restarting kafka' do
+          resource = chef_run.execute('untar kafka binary')
+          expect(resource).to notify('service[kafka]').to(:restart).delayed
+        end
+
       end
     end
   end
