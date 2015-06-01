9edcb0d1c886cb20d017303cb5e349c9c69e9b8c
Scott Medefind <smedefind@optoro.com>
2015-06-01 14:01:15 -0400

    Setting kafka-offset-monitor to 1 min refresh

--- attributes/default.rb
+++ attributes/default.rb
@@ -12,6 +12,7 @@ default['exhibitor']['base_domain'] = 'exhibitor.optoro.io'
 
 default['kafka']['scala_version'] = '2.10'
 default['kafka']['version'] = '0.8.2.1'
+
 # If in EC2, we will mount a ZFS file system as a RAID0
 default['optoro_kafka']['disks'] = ['/dev/sdf']
 override['kafka']['server.properties']['log.dirs'] = '/kafka'
@@ -22,3 +23,5 @@ default['kafka']['server.properties']['kafka.graphite.metrics.port'] = '6008'
 default['kafka']['server.properties']['kafka.graphite.metrics.group'] = "#{node['fqdn']}.kafka"
 default['kafka']['server.properties']['kafka.graphite.metrics.reporter.enabled'] = 'true'
 default['optoro_zfs']['zfs_arc_max'] = (node['memory']['total'].to_i * 0.05 * 1024).round(0).to_s
+
+default['kafka']['offset_monitor']['refresh'] = '1.minutes'
