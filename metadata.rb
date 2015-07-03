6f4a4c5e1984f9c45323b37b4a0e397dae1e0598
Zach Dunn <zdunn@optoro.com>
2015-07-03 14:33:30 -0400

    better ZFS testing

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@optoro.com'
 license 'MIT'
 description 'Installs and configures Kafka'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.4'
+version '0.1.5'
 supports 'ubuntu', '= 14.04'
 
 depends 'apt'
