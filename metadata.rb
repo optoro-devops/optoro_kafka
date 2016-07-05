44e967b8bae0eec9b326bae361f0f7e88c819215
Scott Medefind <smedefind@optoro.com>
2016-07-05 10:59:28 -0400

    Upgrading kafka-graphite to 1.0.4 for bug workaround

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@optoro.com'
 license 'MIT'
 description 'Installs and configures Kafka'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.13'
+version '0.1.14'
 supports 'ubuntu', '= 14.04'
 
 depends 'apt'
