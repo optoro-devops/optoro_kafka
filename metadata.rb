da495a182bfeebb213f05bc5bc2976d61045225f
Jeffrey Gillis <jgillis@optoro.com>
2017-07-25 13:00:36 -0400

    Remove optoro_zfs dependency

--- metadata.rb
+++ metadata.rb
@@ -5,11 +5,10 @@ license 'MIT'
 description 'Installs and configures Kafka'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://github.com/optoro-devops/optoro_kafka'
-version '0.2.3'
+version '0.3.0'
 supports 'ubuntu', '= 14.04'
 
 depends 'apt'
-depends 'optoro_zfs'
 depends 'cerner_kafka', '~> 2.3.0'
 depends 'exhibitor', '~> 0.4.0'
 depends 'optoro_zookeeper'
